0b713b21ac775f82b6f5d83c38300bab29d516f3
Taha Kaplan <66365263+kaplanseunt@users.noreply.github.com>
2021-12-22 15:12:57 +0300

    Add project files.

---
+++ 17.12/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+
+namespace KullanicidanVeriAlma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            // Değişken Tanımlaması:
+            int veri;
+            string ayrac = new string('-', 25);
+            ArrayList list = new ArrayList();
+
+            // Oluşturmuş Olduğumuz ArrayList Atamlarını Yapıyoruz:
+            for (int v = 0; v < 16; v++)
+            {
+                if (v == 0 || v == 4 || v == 8 || v == 12)
+                {
+                    veri = random.Next(65, 90) + 1;
+                    if (list.Contains(veri) == true || list.Contains(Convert.ToChar(veri)) == true)
+                    {
+                        veri = random.Next(75, 85);
+                        list.Add(Convert.ToChar(veri));
+                    }
+                    else
+                        list.Add(Convert.ToChar(veri));
+                }
+                else
+                {
+                    veri = random.Next(15);
+                    if (veri > 9)
+                    {
+                        veri = random.Next(65, 90) + 1;
+                        if (list.Contains(veri) == true || list.Contains(Convert.ToChar(veri)) == true)
+                        {
+                            veri = random.Next(71, 87);
+                            list.Add(Convert.ToChar(veri));
+                        }
+                        else
+                            list.Add(Convert.ToChar(veri));
+                    }
+                    else
+                    {
+                        if (list.Contains(veri) == true || list.Contains(Convert.ToChar(veri)) == true)
+                        {
+                            veri = random.Next(3, 10) - 1;
+                            list.Add(veri);
+                        }
+                        else
+                            list.Add(veri);
+                    }
+                }
+            }
+            Console.Write("{0}\n< Random Lisans Anahtarı >\n{1}\n-> Oluşturulan Anahtar: ", ayrac, ayrac);
+            // Oluşturmuş Olduğumuz Lisans Anahtarını Ekrana Yazdırıyoruz:
+            int r = 0;
+            foreach (var n in list)
+            {
+                Console.Write(n);
+                if (r == 3 || r == 7 || r == 11)
+                {
+                    Console.Write(" - ");
+                }
+                r++;
+            }
+            Console.WriteLine("\n" + ayrac);
+        }
+    }
+}
+    
---
+++ SQLQuery2.sql
@@ -0,0 +1,44 @@
+﻿
+--Select		yorum satırı
+
+--Select ContactName, City, CompanyName  from Customers
+
+--Select ContactName Adi, City Sehir, CompanyName SirketAdi  from Customers		--belirttigim şekilde getirir.
+
+--Select * from Customers where City = 'London'
+
+-- case insensitive
+--sElEct * from Products where CategoryID = 1 or CategoryID=2
+
+--sElEct * from Products where CategoryID = 1 and UnitPrice<10
+
+-- select * from Products order by ProductName
+
+--select * from Products order by CategoryID,ProductName
+
+--select * from Products order by UnitPrice asc  --ascending, dsc descending de düşen sırada dizer
+
+
+select count(*) from Products   -- sayar
+
+select count(*) from Products where CategoryID=2	 
+
+select categoryID, count(*) from products group by CategoryID 		---her bir kategoride o kadar ürün var
+
+select categoryID, count(*) from products group by CategoryID  having count(*)<10
+
+select categoryID, count(*) from products where UnitPrice>20 group by CategoryID having count(*)<10
+
+select Products.ProductID, Products.ProductName, Products.UnitPrice, Categories.CategoryName
+from Products inner join Categories 
+on Products.CategoryID = Categories.CategoryID			--on şart demek
+
+-- DTO data transformation object
+															--inner join tabloları birleştirir
+select * from Products p left join [Order Details] od		--left join solda olup sağda olmayanları da ekle 
+on p.ProductID = od.ProductID			
+
+select * from Customers c left join Orders o
+on c.CustomerID = o.CustomerID							--siparis vermeyen 2 firmayı bulduk
+where o.CustomerID is null
+
